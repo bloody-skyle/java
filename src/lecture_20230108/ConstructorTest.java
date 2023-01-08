220ce6c06f4f2b7020bb0162610d802655076bec
bloody-skyle <sihun0828@gmail.com>
2023-01-08 11:17:12 +0900

    20230108

---
+++ src/lecture_20230108/ConstructorTest.java
@@ -0,0 +1,17 @@
+package lecture_20230108;
+
+class Data1{
+    int value;
+}
+class Data2{
+    int value;
+    Data2(int x){
+        value = x;
+    }
+}
+class ConstructorTest {
+    public static void main(String[] args) {
+        Data1 d1 = new Data1();
+/*        Data2 d2 = new Data2(); */ //에러
+    }
+}
