3d1b878e0d34cf4da258206d40af214641c7facd
JH KIM <panddungee05@gmail.com>
2023-06-25 11:36:38 +0900

    ?:

---
+++ src/lecture_2023335/lecture_01.java
@@ -0,0 +1,14 @@
+package lecture_2023335;
+
+public class lecture_01 {
+    public static void main(String[] args) {
+        int number = 4;
+
+        if (number == 4) {
+            System.out.println("number 는 4입니다.");
+        }
+
+        int number2 = (number == 10) ? 10 : 4;
+        System.out.println(number2);
+    }
+}
